4426d965a6366ed9dd3a314ea777b20d8510da98
hanshenrik <divinity76@gmail.com>
2016-11-07 17:27:42 +0100

    use glibc backtrace() or errors if available

--- misc.c
+++ misc.c
@@ -1,46 +1,70 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
 #if defined(__GLIBC__)
 #include <execinfo.h>
 #endif
 #include "misc.h"
+#define macrobacktrace() { \
+void *array[20]; \
+int traces=backtrace(array,sizeof(array)/sizeof(array[0])); \
+if(traces<=0) { \
+	fprintf(stderr,"failed to get a backtrace!"); \
+} else { \
+backtrace_symbols_fd(array,traces,STDERR_FILENO); \
+} \
+fflush(stderr); \
+}
 
-
-void* emalloc(size_t size) {
-	void* ret = malloc(size);
+void *emalloc(const size_t size) {
+	void *ret = malloc(size);
 	if (unlikely(size && !ret)) {
-		fprintf(stderr, "malloc failed to allocate %zu bytes. terminating...",
-				size);
+		fprintf(stderr,
+				"malloc failed to allocate %zu bytes. errno: %i. strerror: %s. terminating...\n",
+				size, errno, strerror(errno));
+		fflush(stderr);
+		fflush(stdout);
 #if defined(__GLIBC__)
-		//add glibc backtrace() stuff here?
+
+		macrobacktrace()
 #endif
 		exit(EXIT_FAILURE);
 	}
 	return ret;
 }
-void* erealloc(void* ptr, size_t size) {
-	void* ret = realloc(ptr, size);
+void *erealloc(void *ptr, const size_t size) {
+	void *ret = realloc(ptr, size);
 	if (unlikely(size && !ret)) {
-		fprintf(stderr, "realloc failed to allocate %zu bytes. terminating...",
-				size);
+		fprintf(stderr,
+				"realloc failed to allocate %zu bytes. errno: %i. strerror: %s. terminating...\n",
+				size, errno, strerror(errno));
+		fflush(stderr);
+		fflush(stdout);
 #if defined(__GLIBC__)
-		//add glibc backtrace() stuff here?
+		macrobacktrace()
 #endif
 		exit(EXIT_FAILURE);
 	}
 	return ret;
 }
-void* ecalloc(size_t num, size_t size) {
+void *ecalloc(const size_t num, const size_t size) {
 	//im not implementing arbitrary precision integer logic for the error message, feel free to fix it
-	void* ret = calloc(num, size);
+	void *ret = calloc(num, size);
+	fflush(stderr);
+	fflush(stdout);
 	if (unlikely(num > 0 && size > 0 && !ret)) {
-		fprintf(stderr, "calloc failed to allocate %zu bytes. terminating...",
-				num * size);
+		fprintf(stderr,
+				"calloc failed to allocate %zu*%zu bytes. errno: %i. strerror: %s. terminating...\n",
+				num, size, errno, strerror(errno));
 #if defined(__GLIBC__)
-		//add glibc backtrace() stuff here?
+		macrobacktrace()
 #endif
 		exit(EXIT_FAILURE);
 
 	}
 	return ret;
 }
+
